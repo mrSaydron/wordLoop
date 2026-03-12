1fd441622ec1743e044cf266cb6377039c14026c
aleksey.karmanov <aleksei.karmanov@r-x.team>
2026-03-12 10:26:47 +0500

    Исправление отступов

--- app/src/main/java/com/mrsaydron/wordloop/MainActivity.kt
+++ app/src/main/java/com/mrsaydron/wordloop/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.mrsaydron.wordloop.ui.WordLoopApp
 import com.mrsaydron.wordloop.ui.theme.WordLoopTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             WordLoopTheme {
                 WordLoopApp(modifier = Modifier.fillMaxSize())
--- app/src/main/java/com/mrsaydron/wordloop/ui/WordLoopApp.kt
+++ app/src/main/java/com/mrsaydron/wordloop/ui/WordLoopApp.kt
@@ -3,7 +3,10 @@ package com.mrsaydron.wordloop.ui
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.mutableStateOf
@@ -33,7 +36,11 @@ fun WordLoopApp(modifier: Modifier = Modifier) {
     }
 
     CompositionLocalProvider(LocalInAppKeyboard provides keyboardController) {
-        Box(modifier = modifier.fillMaxSize()) {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.systemBars)
+        ) {
             Column(modifier = Modifier.fillMaxSize()) {
                 NavHost(
                     navController = navController,
